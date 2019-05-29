0a215261578f42dbbf7e81d37dba7dd1105a5777
DERUE Paul <deruepaul@yahoo.fr>
2019-05-29 11:27:58 +0200

    Bishop + Rook movements

--- src/main/java/fr/rphstudio/chess/game/Rook.java
+++ src/main/java/fr/rphstudio/chess/game/Rook.java
@@ -19,6 +19,79 @@ public class Rook implements IMove{
     @Override
     public List<IChess.ChessPosition> getPossibleMoves(ChessPosition pos, ChessBoard board) {
         List listPosition = new ArrayList();
+        
+        int posX = pos.x;
+        int posY = pos.y;
+        
+        ChessPosition pos1 = new ChessPosition(posX, posY +1);
+        ChessPosition pos2 = new ChessPosition(posX+1, posY);
+        ChessPosition pos3 = new ChessPosition(posX-1, posY);
+        ChessPosition pos4 = new ChessPosition(posX, posY-1);
+        
+        for(int i = 0; i < 7; i++){
+            if(board.isPosInBoard(pos1) == false){
+                break;
+            }
+            else{
+                if(board.getPiece(pos1) == null){
+                    listPosition.add(pos1);
+                }
+                else if(board.getPiece(pos1).getColor() != board.getPiece(pos).getColor()){
+                    listPosition.add(pos1);
+                    break;
+                }else if(board.getPiece(pos1).getColor() == board.getPiece(pos).getColor()){
+                    break;
+                }   
+            }
+        }
+        for(int i = 0; i < 7; i++){
+            if(board.isPosInBoard(pos2) == false){
+                break;
+            }
+            else{
+                if(board.getPiece(pos2) == null){
+                    listPosition.add(pos2);
+                }
+                else if(board.getPiece(pos2).getColor() != board.getPiece(pos).getColor()){
+                    listPosition.add(pos2);
+                    break;
+                }else if(board.getPiece(pos2).getColor() == board.getPiece(pos).getColor()){
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < 7; i++){
+            if(board.isPosInBoard(pos3) == false){
+                break;
+            }
+            else{
+                if(board.getPiece(pos3) == null){
+                    listPosition.add(pos3);
+                }
+                else if(board.getPiece(pos3).getColor() != board.getPiece(pos).getColor()){
+                    listPosition.add(pos3);
+                    break;
+                }else if(board.getPiece(pos3).getColor() == board.getPiece(pos).getColor()){
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < 7; i++){
+            if(board.isPosInBoard(pos4) == false){
+                break;
+            }
+            else{
+                if(board.getPiece(pos4) == null){
+                    listPosition.add(pos4);
+                }
+                else if(board.getPiece(pos4).getColor() != board.getPiece(pos).getColor()){
+                    listPosition.add(pos4);
+                    break;
+                }else if(board.getPiece(pos4).getColor() == board.getPiece(pos).getColor()){
+                    break;
+                }
+            }
+        }
         return listPosition;
     }
     
